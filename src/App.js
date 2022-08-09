78371d0a8600c8ac9b93557bacb158283fbf9b69
Ryan <rayan.flip@gmail.com>
2022-08-09 16:34:59 +0000

    added route path / and /new-meetup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./styles.css";
 import { Route } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetup from './pages/NewMeetup';
 export default function App() {
   return (
     <div>
       <Route path="/">
           <AllMeetupsPage></AllMeetupsPage>
-          
+      </Route>
+      <Route path='/new-meetup'>
+          <NewMeetup></NewMeetup>
       </Route>
     </div>
   );
