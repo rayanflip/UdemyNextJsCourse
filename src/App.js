67c62c92e03092a143145fcac9d43c0f4bb69bf6
rayanflip <109683081+rayanflip@users.noreply.github.com>
2022-08-09 21:34:26 +0400

    created proper routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./styles.css";
-import { Route } from 'react-router-dom';
-import AllMeetupsPage from './pages/AllMeetups';
-import NewMeetup from './pages/NewMeetup';
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
+import { Fragment } from "react";
 export default function App() {
   return (
     <div>
-      <Route path="/">
-          <AllMeetupsPage></AllMeetupsPage>
-      </Route>
-      <Route path='/new-meetup'>
-          <NewMeetup></NewMeetup>
-      </Route>
+      <Routes>
+        <Fragment>
+          <Route exact path="/" element={<AllMeetupsPage />}></Route>
+
+          <Route path="/new-meetup" element={<NewMeetup />}></Route>
+          <Route path="/favorites" element={<Favorites />}></Route>
+        </Fragment>
+      </Routes>
     </div>
   );
 }
