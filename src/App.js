e59a5f4127692fbac0e5e207c37a62bbc64a7ad3
Ryan <rayan.flip@gmail.com>
2022-08-09 16:21:20 +0000

    Update 5 files, added routing and initial pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
-
+import { Route } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <Route path="/">
+          <AllMeetupsPage></AllMeetupsPage>
+          
+      </Route>
     </div>
   );
 }
